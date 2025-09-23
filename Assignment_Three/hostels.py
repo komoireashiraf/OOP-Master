2fe155c66d55e130b8a181197fb8844726afb7c5
Ashiraf Komoire <komoireashiraf@gmail.com>
2025-09-23 09:05:55 +0300

    classwork coding

---
+++ Assignment_Three/hostels.py
@@ -0,0 +1,10 @@
+class Resident:
+    def _init_(self, name, room_no):
+        self.name = name
+        self.room = room_no
+
+
+class Visitor:
+    def __init__(self, name, Visitor_ID):
+        self.name = name
+        self.Visitor_ID
