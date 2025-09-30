0190a5660277b46244b73a745d5ca831111ef2dd
Ashiraf Komoire <komoireashiraf@gmail.com>
2025-09-30 09:33:49 +0300

    updates

--- Week Three/lecture three/student.py
+++ Week Three/lecture three/student.py
@@ -1,5 +1,18 @@
 class Student:
-    def _init_(self, Name):
+    def __init__(self, Name):
         self.name = Name  # public
-        self._gpa = 3.5  # protected (has one underscore "_")
-        self.__password = "12345"  # private (has two underscore "__")
+        self._gpa = 4.5  # protected attribute (denoted by a single underscore)
+        self.__password = "12345"  # private attribute (denoted by a double underscore)
+
+
+Student_One = Student("Komoire Ashiraf")
+
+# Change name to Muyima Ezrah
+Student_One.name = "Muyima Ezrah"
+# Change GPA to 5.0
+Student_One._gpa = "5.0"
+# Change password to 2025
+Student_One.__password = "2015"
+
+print(Student_One.name)
+print(Student_One._gpa)
