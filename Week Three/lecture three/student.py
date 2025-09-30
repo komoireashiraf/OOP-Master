a2ea1ccac10fb147e7be7155b1e53da6f1a78cf1
Ashiraf Komoire <komoireashiraf@gmail.com>
2025-09-30 08:52:33 +0300

    updates

--- Week Three/lecture three/student.py
+++ Week Three/lecture three/student.py
@@ -1,5 +1,5 @@
 class Student:
     def _init_(self, Name):
         self.name = Name  # public
-        self._gpa = 3.5  # protected (has on underscore "_")
+        self._gpa = 3.5  # protected (has one underscore "_")
         self.__password = "12345"  # private (has two underscore "__")
