ef69b57467c2c4e5932bd2990c68fa9aa229670d
Ashiraf Komoire <komoireashiraf@gmail.com>
2025-10-14 09:54:04 +0300

    updates

--- Week Three/person.py
+++ Week Three/person.py
@@ -12,12 +12,21 @@ class Student(Person):
         self.program = program
         self.year = year
 
+    def introduce(self):
+        super().introduce() + f" I am studying {self.program} in year {self.year}."
+
 
 class Lecturer(Person):
     def _init_(self, name, program, department):
         super()._init_(name)
         self.program = program
         self.department = department
+
+    def introduce(self):
+        (
+            super().introduce()
+            + f" I teach {self.program} in the {self.department} department."
+        )
 
 
 p = Person("Ashiraf")
